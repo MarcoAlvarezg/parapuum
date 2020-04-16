421ea5e63e092f198cd59ae75b66de2c0c7e0422
Marco Antonio Alvarez Guzman <MARCO.ANTONIO.ALVAREZ.GUZMAN@ibm.com>
2020-04-16 16:35:36 -0500

    CambiosMayores

--- protected/javascript/prod.js
+++ protected/javascript/prod.js
@@ -23,8 +23,8 @@ const I = {
         producto2 = en.descripcion;
         producto3 = en.img;
         //console.log(producto3);
-        prod.innerHTML += '<div class="column"><div class="content"><h3>Producto</h3>'+producto1+'<br><br>'+'<h3>Decripción</h3><p>'+producto2+'</p><br><br>' + '<a><img src="'+producto3+'" width="200" height="200" alt="'+en.categoria+'"></a><br><br></div></div>';
-
+        prod.innerHTML += '<div class="columnprod"><div class="contentprod"><h3>Producto</h3><img src="'+producto3+'" alt="'+en.categoria+' class="imagetable" height="300" width="300" ><div class=overlaytable><div class="texttable">'+producto1+'<br><a href="/protected/protected2.html"><button class="b">Detalles</button></a><br><br><a href="/protected/protected3.html"><button class="b">Agregar al carrito</button></a></div></div></div></div>';
+        //prod.innerHTML += '<div class="column"><div class="content"><h3>Producto</h3>'+producto1+'<br><br>'+'<h3>Decripción</h3><p>'+producto2+'</p><br><br>' + '<a><img src="'+producto3+'" width="200" height="200" alt="'+en.categoria+'"></a><br><br></div></div>';
        }
     })
 });
