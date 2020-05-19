79452351ce648c025f3522a1fb707359b4df742d
Ant Alvarez <MARCO.ANTONIO.ALVAREZ.GUZMAN@ibm.com>
2020-05-19 14:44:09 -0500

    fav

--- protected/javascript/cloudant.js
+++ protected/javascript/cloudant.js
@@ -109,13 +109,13 @@ var n=0;
         var totalEH = eh.reduce((sum, value) => (typeof value.hours == "number" ? sum + value.hours : sum), 0);
         //$("#"+entry._id).html(totalEH+" / 100");   
         console.log(totalEH);
-       // var k =document.getElementById(entry._rev);
-        //n=(totalEH/40)*180;
-      //  k.style.background="linear-gradient("+n+"deg, rgb(0, 0, 255) 50%, rgba(0, 0, 0, 0.2) 50%)";
+       /* var k =document.getElementById(entry._rev);
+        n=(totalEH/40)*180;
+        k.style.background="linear-gradient("+n+"deg, rgb(0, 0, 255) 50%, rgba(0, 0, 0, 0.2) 50%)";
         })
       }
-      );
-    } */
+      );*/
+    },)
 
   // intercept the click on the submit button, add the guestbook entry and
   // reload entries on success
